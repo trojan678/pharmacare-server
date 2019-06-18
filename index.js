8f32f4cffed914e66014d50c5e6aa82384b7aeca
peter <p19njoroge@gmail.com>
2019-06-18 13:11:12 +0300

    api

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+require("dotenv").config();
 const express = require ("express");
+const mysql = require("mysql");
 const app = express();
-app.get("/", function (req, res){
-    //logic
-    res.send("hello world");
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+});
+app.get("/api/patient",(req, res) => {
+    pool.query("SELECT * FROM patient",(error, rows) =>{
+        if (error) {
+            return res.status(500).json({error});
+        }
+
+        res.json(rows);
+    });
 });
 app.listen(600, function (){
     console.log("App listening on port 600");
